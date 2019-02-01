551a5ca4f80eb6161ed9919d4797532890cc6444
Rin <radysurya.agus@gmail.com>
2019-02-02 00:01:20 +0700

    04 Finish (#3)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import FormProperty from 'components/FormProperty'
+import Container from 'reactstrap/lib/Container'
+import Row from 'reactstrap/lib/Row'
+import Col from 'reactstrap/lib/Col'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    data: {
+      address: '',
+      name: '',
+    },
+  }
+
+  constructor(props) {
+    super(props)
+    this.handleAction = this.handleAction.bind(this)
+  }
+
+  handleAction(values) {
+    const { data } = this.state
+    Object.assign(data, values)
+    this.setState({ data })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container>
+        <Row>
+          <Col sm="12">
+            <h1>{process.env.REACT_APP_NAME}</h1>
+            <hr />
+            <FormProperty action={this.handleAction} />
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
-
-export default App;
